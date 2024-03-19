f8ba6d18f570390cd2408e3fbfa962b623e987d8
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-19 20:01:57 +0400

    created layout for main pages

---
+++ layouts/main.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <MobileHeader />
+  <Sidebar class="hidden lg:flex" />
+  <main class="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+    <div class="max-w-[1056px] mx-auto pt-6 h-full">
+      <slot />
+    </div>
+  </main>
+</template>
