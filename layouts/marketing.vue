f5d2afe9672d8d6ce5957cf438ed3ea91e8fa2af
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-19 20:01:36 +0400

    created layout for marketing page

---
+++ layouts/marketing.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Header from "@/components/marketing/Header.vue";
+import Footer from "@/components/marketing/Footer.vue";
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Header />
+    <main class="flex-1 flex flex-col items-center justify-center">
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
