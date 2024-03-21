f2bfd493cb0597d42512a1dc708c97d03a063b3e
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-21 19:56:34 +0400

    created MobileHeader and MobileSidebar components

---
+++ components/MobileHeader.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav
+    class="lg:hidden px-6 h-[50px] flex items-center bg-green-500 border-b fixed top-0 w-full z-50"
+  >
+    <MobileSidebar />
+  </nav>
+</template>
---
+++ components/MobileSidebar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-vue-next";
+import Sidebar from "@/components/Sidebar.vue";
+</script>
+
+<template>
+  <Sheet>
+    <SheetTrigger>
+      <Menu class="text-white" />
+    </SheetTrigger>
+    <SheetContent class="p-0 z-[100]" side="left">
+      <Sidebar />
+    </SheetContent>
+  </Sheet>
+</template>
