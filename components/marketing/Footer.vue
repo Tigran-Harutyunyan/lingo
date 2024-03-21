297d56032021950c0546761344ed97e26a3ecc3f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-21 19:53:58 +0400

    header and footer components for marketing page

---
+++ components/marketing/Footer.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <footer class="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+    <div
+      class="max-w-screen-lg mx-auto flex items-center justify-evenly h-full"
+    >
+      <Button size="lg" variant="ghost" class="w-full">
+        <NuxtImg
+          src="/hr.svg"
+          alt="Croatian"
+          :height="32"
+          :width="40"
+          class="mr-4 rounded-md"
+        />
+        Croatian
+      </Button>
+      <Button size="lg" variant="ghost" class="w-full">
+        <NuxtImg
+          src="/es.svg"
+          alt="Spanish"
+          :height="32"
+          :width="40"
+          class="mr-4 rounded-md"
+        />
+        Spanish
+      </Button>
+      <Button size="lg" variant="ghost" class="w-full">
+        <NuxtImg
+          src="/fr.svg"
+          alt="French"
+          :height="32"
+          :width="40"
+          class="mr-4 rounded-md"
+        />
+        French
+      </Button>
+      <Button size="lg" variant="ghost" class="w-full">
+        <NuxtImg
+          src="/it.svg"
+          alt="Italian"
+          :height="32"
+          :width="40"
+          class="mr-4 rounded-md"
+        />
+        Italian
+      </Button>
+      <Button size="lg" variant="ghost" class="w-full">
+        <NuxtImg
+          src="/jp.svg"
+          alt="Japanese"
+          :height="32"
+          :width="40"
+          class="mr-4 rounded-md"
+        />
+        Japanese
+      </Button>
+    </div>
+  </footer>
+</template>
---
+++ components/marketing/Header.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { Loader } from "lucide-vue-next";
+
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  ClerkLoading,
+  ClerkLoaded,
+  UserButton,
+} from "vue-clerk";
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <header class="h-20 w-full border-b-2 border-slate-200 px-4">
+    <div
+      class="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full"
+    >
+      <div class="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+        <NuxtImg src="/mascot.svg" :height="40" :width="40" alt="Mascot" />
+        <h1 class="text-2xl font-extrabold text-green-600 tracking-wide">
+          Lingo
+        </h1>
+      </div>
+      <ClerkLoading>
+        <Loader class="h-5 w-5 text-muted-foreground animate-spin" />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton
+            v-slot="{ handler }"
+            mode="modal"
+            afterSignInUrl="/learn"
+            afterSignUpUrl="/learn"
+          >
+            <Button @click="handler" size="lg" variant="ghost"> Login </Button>
+          </SignInButton>
+        </SignedOut>
+      </ClerkLoaded>
+    </div>
+  </header>
+</template>
