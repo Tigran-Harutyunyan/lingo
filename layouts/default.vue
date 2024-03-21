5d8d0fc0f8a0aa37b606f1a33b203d398ef04fac
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-21 20:03:03 +0400

    layouts

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { ClerkLoaded } from "vue-clerk";
+</script>
+<template>
+  <ClerkLoaded>
+    <slot></slot>
+  </ClerkLoaded>
+</template>
---
+++ layouts/lesson.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col h-full">
+    <div class="flex flex-col h-full w-full">
+      <slot />
+    </div>
+  </div>
+</template>
