43f391d8efbdd0c46e350d23979f94b1a8d87dfa
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-21 20:02:17 +0400

    created useApi composable to store promises

---
+++ composables/useApi.ts
@@ -0,0 +1,54 @@
+import type {
+    UserProgress,
+    Subscription,
+    UnitWithLessons,
+    courseProgress,
+    LessonWithExtras,
+    User
+} from "@/db/queries";
+
+export const useApi = () => {
+
+    const getUserProgress = () => {
+        return $fetch<UserProgress | null>(`/api/progress`)
+    }
+
+    const getUnits = () => {
+        return $fetch<UnitWithLessons[] | null>(`/api/units`)
+    }
+
+    const getUserSubscriptions = () => {
+        return $fetch<Subscription | null>(`/api/subscription`)
+    }
+
+    const getLessonPercentage = () => {
+        return $fetch<number>(`/api/lesson-percentage`)
+    }
+
+    const getCoursProgress = () => {
+        return $fetch<courseProgress>(`/api/course/progress`)
+    }
+
+    const getLesson = (lessonId?: number) => {
+        return lessonId ? $fetch<LessonWithExtras[] | null>(`/api/lesson?lessonId=${lessonId}`) : $fetch<LessonWithExtras[] | null>(`/api/lesson`)
+    }
+
+    const getTopUsers = () => {
+        return $fetch<User[]>(`/api/top-users`)
+    }
+
+    const refillHearts = () => {
+        return $fetch<User[]>(`/api/refill-hearts`)
+    }
+
+    return {
+        getUserProgress,
+        getUnits,
+        getUserSubscriptions,
+        getLessonPercentage,
+        getCoursProgress,
+        getLesson,
+        getTopUsers,
+        refillHearts
+    }
+}
