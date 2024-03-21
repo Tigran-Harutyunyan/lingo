5bbe2fa8cca5a74e405a8e0b176de477c7663aa9
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-21 19:46:14 +0400

    created Practice modal

---
+++ components/modals/PracticeModal.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import { Button } from "@/components/ui/button";
+
+import { useModalsStore } from "@/stores/modals";
+
+const { type, isOpen } = storeToRefs(useModalsStore());
+
+const { onClose } = useModalsStore();
+
+const isModalOpen = computed(() => {
+  return isOpen.value && type.value === "practice";
+});
+</script>
+
+<template>
+  <Dialog v-model:open="isModalOpen" @update:open="onClose()">
+    <DialogContent class="max-w-md">
+      <DialogHeader>
+        <div class="flex items-center w-full justify-center mb-5">
+          <NuxtImg src="/heart.svg" alt="Heart" :height="100" :width="100" />
+        </div>
+        <DialogTitle class="text-center font-bold text-2xl">
+          Practice lesson
+        </DialogTitle>
+        <DialogDescription class="text-center text-base">
+          Use practice lessons to regain hearts and points. You cannot loose
+          hearts or points in practice lessons.
+        </DialogDescription>
+      </DialogHeader>
+      <DialogFooter class="mb-4">
+        <div class="flex flex-col gap-y-4 w-full">
+          <Button variant="primary" class="w-full" size="lg" @click="onClose">
+            I understand
+          </Button>
+        </div>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
+</template>
