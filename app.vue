99914a4d85a6419a0341f8109615101f539ca541
Tigran <harutyunyan.tigran1975@gmail.com>
2024-03-21 20:47:03 +0400

    added progress indicator

--- app.vue
+++ app.vue
@@ -3,6 +3,8 @@ import { Toaster } from "@/components/ui/sonner";
 import ModalsContainer from "@/components/modals/ModalsContainer.vue";
 </script>
 <template>
+  <NuxtLoadingIndicator color="#4ade80" :height="6" />
+
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
